0829bbab6dc0059d4ae8dea1443855608b9b0f46
Mpreyzner <maria.preyzner@gmail.com>
2018-12-05 20:28:21 +0100

    Added more functionality to scraper scaffold

--- main/scraper/post_scraper.py
+++ main/scraper/post_scraper.py
@@ -1,48 +1,55 @@
 import bs4
 import requests
+from urllib.parse import urlparse, urlunparse
 
 # check if post is already scraped (check by title in db)
-# save in db
-# emit event https://github.com/transifex/django-events
+# save in db -> author as well as post
+# emit event https://github.com/transifex/django-events with post content and details
 
-headers = {}
 url = 'https://teonite.com/blog'
-url_prefix = 'https://teonite.com'  # this can be solved cleaner because there is absolute url and relative url
 title_class = 'post-title'
-
-author_class = 'author-content'
+next_page_class = 'older-posts'
 
 
 def get_elements_from_page(url, css_class):
-    response = requests.get(url, headers=headers)
+    response = requests.get(url)
     response.raise_for_status()
     soup = bs4.BeautifulSoup(response.text, 'lxml')
     elems = soup.select('.' + css_class)
     return elems
 
 
-def get_posts(url, base_url, page=1):
-    # todo please build urls in some legit way
-    # return tuple name -> url
+def get_posts(url, page=1):
+    parsed_url = urlparse(url)
     posts = get_elements_from_page(url, title_class)
     for a in posts:
         title = a.text.strip()
         # at this point we could check is post saved in db
-        print(title)
+        # print(title)
         post_url = a.find('a')['href']
-        content = get_post_content(base_url + post_url)
-        print(content)
+        content, author = get_post_details(
+            (parsed_url.scheme + '://' + parsed_url.netloc + post_url))  # use some function for url
+        # print(content)
+        # print(author)
+        #emit event here
     return posts
 
 
-def get_post_content(post_url):
+def get_post_details(post_url):
     content_class = 'post-content'
-    return get_elements_from_page(post_url, content_class)[0].text.strip() #dokoptuj autora
+    author_class = 'author-content'
+    author = get_elements_from_page(post_url, author_class)[0].find('h4').text.strip()
+    content = get_elements_from_page(post_url, content_class)[0].text.strip()
+    return content, author
 
 
-get_posts(url, url_prefix)
-
-
-
-
-# if no post left then go to class older-posts and follow link until there is no older post
+parsed_url = urlparse(url)
+# https://teonite.com/blog/page/2/
+# or just change page parameter ??
+finished = False
+while not finished:
+    get_posts(url)
+    next_page_path = get_elements_from_page(url, next_page_class)[0]['href']
+    # check if given index exists, if it doesn't then we're finished
+    url = parsed_url.scheme + '://' + parsed_url.netloc + next_page_path
+    print(url)
